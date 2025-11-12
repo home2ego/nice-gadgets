acf89d0e4535a3029122354e39e3cc6ab9a154d8
Artem Zhyrnyi <ic3ego@gmail.com>
2025-11-12 20:32:06 +0200

    change the load step to 12

--- src/modules/shared/components/ProductsSection/constants.ts
+++ src/modules/shared/components/ProductsSection/constants.ts
@@ -3,4 +3,4 @@ import type { PageOption, SortOption } from "../../types/select";
 export const INITIAL_SORT: SortOption = "newest";
 export const INITIAL_PER_PAGE: PageOption = "all";
 export const INITIAL_PAGE = 1;
-export const LOAD_STEP = 24;
+export const LOAD_STEP = 12;
